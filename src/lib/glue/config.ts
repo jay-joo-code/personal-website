c0fe02f453aebc4919519a47aac32699db176285
Jay Joo <jj534@cornell.edu>
2023-12-04 15:00:46 -0500

    add navigation

--- src/lib/glue/config.ts
+++ src/lib/glue/config.ts
@@ -1,7 +1,20 @@
 import type { IPrivateNav, IPublicNav } from '$lib/types/glue/nav.type';
 
-export const APP_NAME = "Hi, I'm Jay";
-export const PUBLIC_NAVS: IPublicNav[] = [];
+export const APP_NAME = 'Jay';
+export const PUBLIC_NAVS: IPublicNav[] = [
+	{
+		label: 'Experience',
+		path: '#experience'
+	},
+	{
+		label: 'Projects',
+		path: '#projects'
+	},
+	{
+		label: 'Contact',
+		path: '#contact'
+	}
+];
 export const PRIVATE_NAVS: IPrivateNav[] = [];
 export const IS_GOOGLE_AUTH_ONLY = true;
 export const IS_ENFORCE_CORNELL_EMAIL = true;
